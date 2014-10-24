1a0bcc8e8f1abc15b321247602832687247ecc5e
geovannesdi@gmail.com <geovannesdi@gmail.com@5fa5096d-a7b5-05c0-8163-a1a5cd34e441>
2014-10-24 14:45:05 +0000

    Teste de comite, em classe teste

---
+++ trunk/src/logica_plft5/Teste.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package logica_plft5;
+
+/**
+ *
+ * @author Geovanne Barreto
+ */
+public class Teste {
+    
+}
